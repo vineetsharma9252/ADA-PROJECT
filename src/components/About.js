fc7edb5f97dab026d2ff6b3f36a12574c8bbb7b0
vineet <Mastergenos228@gmail.com>
2025-03-08 18:57:33 +0530

    8March 7:00

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+const About = memo(function About(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+})
+
+About.propTypes = {
+
+}
+
+export default About

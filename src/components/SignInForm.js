8b597f2517e867f66f37aec2633e9d57ee6ee07c
viveksharma112232 <155580719+viveksharma112232@users.noreply.github.com>
2025-03-10 11:15:06 +0530

    10/03/2025 11:14 AM  SignInForm.js Checked

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -47,14 +47,14 @@ export default function SignInForm() {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 my-20">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-1 lg:px-8 my-1">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
             Sign in to your account
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={handleSubmit} method="POST" className="space-y-6">
             <div>
               <label
